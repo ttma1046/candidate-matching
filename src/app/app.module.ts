9d11cad5bb14da3a3c3862b6fd2270a82ca89ebd
Chandler Fang <chandlerfang@gmail.com>
2018-03-11 14:39:49 +1100

    Create candidate module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CandidateModule } from './candidate/candidate.module';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CandidateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
