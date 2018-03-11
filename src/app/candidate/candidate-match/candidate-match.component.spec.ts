70ab79b2a6e12e6b74c94e2e657c75777172b8e5
Chandler Fang <chandlerfang@gmail.com>
2018-03-11 17:39:13 +1100

    Skip the unit tests for app.component.

--- src/app/candidate/candidate-match/candidate-match.component.spec.ts
+++ src/app/candidate/candidate-match/candidate-match.component.spec.ts
@@ -1,14 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import { CoreModule } from '../../core/core.module';
+import { TreeTableModule } from 'primeng/treetable';
 
 import { CandidateMatchComponent } from './candidate-match.component';
 
 describe('CandidateMatchComponent', () => {
-  let component: CandidateMatchComponent;
+let component: CandidateMatchComponent;
   let fixture: ComponentFixture<CandidateMatchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CandidateMatchComponent ]
+      declarations: [ CandidateMatchComponent ],
+      imports: [ TreeTableModule, CoreModule ]
     })
     .compileComponents();
   }));
@@ -22,4 +27,11 @@ describe('CandidateMatchComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('On load', () => {
+    it('should have the table element', () => {
+      const treeTable = fixture.debugElement.query(By.css('p-treetable'));
+      expect(treeTable).toBeTruthy();
+    });
+  });    
 });
--- src/app/core/services/core.service.spec.ts
+++ src/app/core/services/core.service.spec.ts
@@ -1,15 +1,53 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpErrorResponse, HttpClientModule, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { TreeNode } from 'primeng/api';
+
+import { Job } from '../models/job';
 
 import { CoreService } from './core.service';
 
 describe('CoreService', () => {
+  let httpClientSpy: { get: jasmine.Spy };
+  let coreService: CoreService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CoreService]
+      imports: [ HttpClientModule ],
+      providers: [
+        CoreService
+      ]
     });
+
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    coreService = new CoreService(<any> httpClientSpy);
   });
 
-  it('should be created', inject([CoreService], (service: CoreService) => {
-    expect(service).toBeTruthy();
-  }));
+
+  it('can instantiate service via DI', () => {
+      coreService = TestBed.get(CoreService);
+      expect(coreService instanceof CoreService).toBe(true);
+  });
+
+  it('can instantiate service with "new"', () => {
+    const http = TestBed.get(HttpClient);
+    expect(http).not.toBeNull('http should be provided');
+    let service = new CoreService(http);
+    expect(service instanceof CoreService).toBe(true, 'new service should be ok');
+  });
+
+
+  it('should return expected jobs (HttpClient called once)', () => {
+    const expectedJobs: TreeNode[] = [];
+
+    httpClientSpy.get.and.returnValue(of(expectedJobs));
+
+    coreService.getJobs().subscribe(
+      jobs => expect(jobs).toEqual(expectedJobs, 'expected Jobs'),
+      fail
+    );
+
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+  });
 });
