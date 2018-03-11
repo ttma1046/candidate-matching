02ae322f36753a2ec5c5a4b77a422249437ec9c0
Chandler Fang <chandlerfang@gmail.com>
2018-03-11 15:54:48 +1100

    Service call to get the jobs to map to treenode array.

--- src/app/core/services/core.service.ts
+++ src/app/core/services/core.service.ts
@@ -1,30 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { TreeNode } from 'primeng/api';
 
 @Injectable()
 export class CoreService {
-
+  private baseUrl = 'http://private-76432-jobadder1.apiary-mock.com/';
   constructor(private http: HttpClient) { }
 
   getJobs(): Observable<TreeNode[]> {
-      return Observable.of([{
-                data: {
-                    name: 'Test',
-                    company: 'Test',
-                    skills: 'Test'
-                },
-                children:[
-                {  
-                    data: {
-                        name: 'Test Sub',
-                        company: 'Test Sub',
-                        skills: 'Test Sub'
-                    }
-                }
-                ]
-            } as TreeNode]);
+        return this.http.get<TreeNode[]>(`${this.baseUrl}/jobs`).pipe(
+            map((jobs: TreeNode[]) => {
+                return jobs.map(job => ({
+                    data: job,
+                    leaf: false
+                } as TreeNode)
+                );
+            })
+        );
   }
 }
